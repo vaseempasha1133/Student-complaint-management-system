43ee591751b47f37f34a7889920a4aee8969e2b1
vaseempasha1133 <shaikvaseempasha996@gmail.com>
2026-07-19 22:25:11 +0530

    Updated railway complaint system

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,8 @@
-MYSQL_HOST = "localhost"
+MYSQL_HOST = "tokaido.proxy.rlwy.net"
+MYSQL_PORT = 3306          
 MYSQL_USER = "root"
-MYSQL_PASSWORD = "Vaseem@123"
-MYSQL_DB = "student_complaint_system"
+MYSQL_PASSWORD = "CLxaEaKAbjWcIVffDitWkLuIJLrREfkh"
+MYSQL_DB = "railway"
 MYSQL_CURSORCLASS = "DictCursor"
 
 SECRET_KEY = "student_complaint_secret_key"
