4e76d91dc87d814a637d692a9185f289c804b894
vaseempasha1133 <shaikvaseempasha996@gmail.com>
2026-07-21 19:32:19 +0530

    Test automatic deployment

--- backend/app.py
+++ backend/app.py
@@ -31,7 +31,7 @@ app.register_blueprint(complaints)
 app.register_blueprint(admin)
 @app.route("/")
 def home():
-    return "🚀 Automatic Deployment is Working!"
+    return "This is Vaseem Automatic Deployment is Working!"
 
 import os
 
